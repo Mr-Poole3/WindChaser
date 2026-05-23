993489b6406886e2ba12c1bdf1b1ff4ad0e5bf59
LiPan <lipan@adsit.cn>
2026-05-24 02:15:15 +0800

    Initial Commit

---
+++ WindChaser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WindChaser
+//
+//  Created by Mac on 2026/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WindChaser/WindChaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindChaserApp.swift
+//  WindChaser
+//
+//  Created by Mac on 2026/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WindChaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
